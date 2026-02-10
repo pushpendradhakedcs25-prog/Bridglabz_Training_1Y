6df15ceefd67dd28a64928a2b8214079ed723169
Pushpendra Dhakad <pushpendra.dhakad_cs25@gla.ac.in>
2026-02-10 18:16:36 +0530

    remove

--- Programing_constract/Nth_sum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-    class Nth{
-	public static void main(String []args){
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter number");
-	int n = input.nextInt();
-int 	sum = n*(n+1)/2 ;
-System.out.println("sum of ("+n+") natural number is ("+sum+")");
-	}}
